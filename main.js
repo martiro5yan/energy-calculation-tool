969420200c5a77b2194e4a99809716ab10dfd375
martiri5yan <martiro5yan@vk.com>
2024-06-15 16:37:07 +0300

    ect v2, style update

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 function Calculate(post){
     
+    var count_impuls = 5
+    var impuls = document.getElementById('impuls')
+    if (impuls.value.length > 0)
+        count_impuls = impuls.value
+
     var A = document.getElementById('A')
     var B = document.getElementById('B')
     var C = document.getElementById('C')
@@ -7,14 +12,14 @@ function Calculate(post){
 
     var kVa = 0.22 * ABC
     var kVt = kVa * 0.9
-    var t = 3600 * 5 / post / kVt
+    var t = 3600 * count_impuls / post / kVt
 
     kVa = kVa.toFixed(2)
     kVt = kVt.toFixed(2)
     t = t.toFixed(2)
 
 
-    document.getElementById('kVa').innerHTML = "kVa = " + kVa
-    document.getElementById('kVt').innerHTML = "kVt = " + kVt
-    document.getElementById('t').innerHTML = "time = " + t
+    document.getElementById('kVa').innerHTML = "кВа = " + kVa
+    document.getElementById('kVt').innerHTML = "кВт = " + kVt
+    document.getElementById('t').innerHTML = "Время = " + t
 }
